fb54b555de0d7b5548c7aed12989a96cec388b34
kevan.chen <kevan.chen@sinopa.com>
2020-07-23 15:45:49 +0800

    AutoSizeColumn Set

--- ConsoleNPOI/Program.cs
+++ ConsoleNPOI/Program.cs
@@ -43,23 +43,39 @@ namespace ConsoleNPOI
 			ws.CreateRow(0);//第一行為欄位名稱
 			ws.GetRow(0).CreateCell(0).SetCellValue("name");
 			ws.GetRow(0).CreateCell(1).SetCellValue("score");
+		
 
 			// data
 			ws.CreateRow(1);//第二行之後為資料
 			ws.GetRow(1).CreateCell(0).SetCellValue("abey");
+			ws.AutoSizeColumn(0);
 			ws.GetRow(1).CreateCell(1).SetCellValue(85);
+			ws.AutoSizeColumn(1);
+
 			ws.CreateRow(2);
-			ws.GetRow(2).CreateCell(0).SetCellValue("tina");
+			ws.GetRow(2).CreateCell(0).SetCellValue("tina111111111111111111111");
+			ws.AutoSizeColumn(0);
 			ws.GetRow(2).CreateCell(1).SetCellValue(82);
+			ws.AutoSizeColumn(1);
+
 			ws.CreateRow(3);
 			ws.GetRow(3).CreateCell(0).SetCellValue("boi");
+			ws.AutoSizeColumn(0);
 			ws.GetRow(3).CreateCell(1).SetCellValue(84);
+			ws.AutoSizeColumn(1);
 			ws.CreateRow(4);
-			ws.GetRow(4).CreateCell(0).SetCellValue("hebe");
+
+			ws.GetRow(4).CreateCell(0).SetCellValue("hebe22222");
+			ws.AutoSizeColumn(0);
 			ws.GetRow(4).CreateCell(1).SetCellValue(86);
+			ws.AutoSizeColumn(1);
+
 			ws.CreateRow(5);
 			ws.GetRow(5).CreateCell(0).SetCellValue("paul");
+			ws.AutoSizeColumn(0);
 			ws.GetRow(5).CreateCell(1).SetCellValue(82);
+			ws.AutoSizeColumn(1);
+
 
 			FileStream file = new FileStream(@"C:\Users\011714\Desktop\TEST\npoi.xls", FileMode.Create);//產生檔案
 			wb.Write(file);
